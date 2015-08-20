e5355b09858ef2765b9385b1e5e2d037c01692cc
Greg Silverman <greg.silverman@memeo-inc.com>
2015-08-19 18:20:29 -0700

    More experimentation.

--- unuvero/snippets/magni_auth.py
+++ unuvero/snippets/magni_auth.py
@@ -3,23 +3,25 @@ Created on Aug 19, 2015
 
 @author: gsilverman
 '''
-
-import logging
+import pprint
 import requests
 import collections
+import json
 
-try:
-    import http.client as http_client
-except ImportError:
-    # Python 2
-    import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
-
-logging.basicConfig() 
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
+if False:
+    try:
+        import http.client as http_client
+    except ImportError:
+        # Python 2
+        import httplib as http_client
+    http_client.HTTPConnection.debuglevel = 1
+    
+    import logging
+    logging.basicConfig() 
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
 
 
 payload = collections.OrderedDict()
@@ -30,7 +32,9 @@ payload['web_session'] = 'false'
 
 
 r = requests.post('https://c1.memeo.com/thor/auth/magni',params=payload)
-for i in r:
-    print i
+j = json.JSONDecoder().decode(r.text)
+
+print j['magni_token']
+print j['links']['home']
 
 

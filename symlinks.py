eaff9aac4b995ddac1bc6c4703451a00b00a16a8
Greg Silverman <greg.silverman@memeo-inc.com>
2015-08-27 17:52:25 -0700

    correctly create symbolic links

--- symlinks.py
+++ symlinks.py
@@ -1,22 +1,21 @@
 import sys
 import os
 import os.path
+import collections
 
-cmd_format_string = 'ln -s %s %s.py'
+dirmap = collections.OrderedDict()
 
 for root, dirs, files in os.walk('.'):
-
-    for f in files:
-
-        if f.startswith('wscript') and not f.endswith('.py') and f+'.py' not in files:
-            
-            src = os.path.abspath(root+'/'+f)
-            try:
-                pass
-                cmd = cmd_format_string % (src,src)
-                #print cmd
-                os.system(cmd)
-            except OSError as e:
-                if e.errno != 17:
-                    raise e
-
+	for f in files:
+		if f.startswith('wscript'):
+			
+			if not root in dirmap:
+				dirmap[root] = []
+			dirmap[root].append(f)
+for d in dirmap:
+	olddir = os.getcwd()
+	os.chdir(d)
+	for f in dirmap[d]:
+		if f.startswith('wscript') and not f.endswith('.py') and not f+'.py' in files:
+			os.symlink(f,f+'.py')
+	os.chdir(olddir)
